8d15c73a9a8c8e376d731be88d72b4b1f4d8f76e
victor-iyiola <javafolabi@gmail.com>
2017-08-05 13:44:36 +0100

    Writing simple functions

---
+++ index.js
@@ -0,0 +1,67 @@
+const path = require('path'),
+  fs = require('fs');
+
+const ignored_files = ['package-lock.json', 'font-awesome.min.js', 'font-awesome.css',
+    'bootstrap-grid.min.css', 'bootstrap.css', 'bootstrap.min.css', 'dropzone.css',
+    'owl.carousel.min.js', 'jquery.min.js', 'jquery-1.10.2.js', 'owl.carousel.min.css',
+    'owl.theme.default.min.css', 'owl.video.play.png', 'pretty.min.css',
+    'jquery_upload.min.js', 'datatables.min.js', 'dataTables', 'datatables.min.css',
+    'bootstrap.min.js', 'dropzone.js', 'tether.min.js', 'wow.min.js', 'font-awesome.min.css',
+  ],
+  ignored_folders = [
+    'node_modules', 'uploads', 'fonts', 'dataTables'
+  ],
+  ignored_extensions = [
+    'pdf', 'txt', 'md', 'ini', 'gif', 'jpg', 'png', 'jpeg', 'ttf'
+  ],
+  allowed_extensions = [
+    'html', 'css', 'js', 'php', 'scss',
+  ];
+
+let linesOfCode = 0;
+const directories = [],
+  files = [];
+
+/**
+ * Gets the extension of a file
+ * @param {string} file 
+ */
+function extension(file) {
+  return path.extname(file).substr(1)
+}
+
+/**
+ * Adds file to files or directories as appropriate.
+ * @param {path} file 
+ */
+function addFile(file) {
+  fs.stat(file, function (err, stat) {
+    if (err) {
+      console.error(err.message);
+    } else {
+      if (stat.isFile()) {
+        res = files.filter(function (f) {
+          return f === file;
+        });
+        if (res.length === 0)
+          files.push(file);
+      } else {
+        directories.push(file);
+      }
+    }
+  });
+}
+
+/**
+ * Counts how many lines of code are there.
+ * And adds it to the linesOfCode variable.
+ * @param {path} file 
+ */
+function countLines(file) {
+  fs.readFile(file, 'utf8', function (err, data) {
+    if (err) return console.error('Error: ', err);
+    linesOfCode += data.split('\n').length;
+  });
+}
+
+countLines('package.json');
